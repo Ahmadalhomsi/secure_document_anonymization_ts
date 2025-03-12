c41d1cf2d3a71f550106be79d447bfab189b2228
Ahmadalhomsi <tarikuka.aa@gmail.com>
2025-03-12 14:06:59 +0300

    FastAPI init

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+### Create FastAPI instance with custom docs and openapi url
+app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
+
+@app.get("/api/py/helloFastApi")
+def hello_fast_api():
+    return {"message": "Hello from FastAPI"}
+
+
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Next.js default port
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
